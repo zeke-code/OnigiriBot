5d1ae1e8ca116fdcff893240d8878c1336c861ba
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-03-06 01:51:37 +0100

    Fix error event

--- src/events/error.js
+++ src/events/error.js
@@ -5,6 +5,5 @@ module.exports = {
   name: Events.Error,
   async execute(interaction, error) {
     logger.error(`A critical error occurred: ${error}`);
-    return interaction.reply('A critical error occurred. Try again later.');
   },
 };
