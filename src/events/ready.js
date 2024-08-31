fe9f574425b9ce2d0a052e6e7655cd6783a5d339
zeke-code <116798879+DemaControlsUs@users.noreply.github.com>
2024-08-31 18:54:35 +0200

    Minor changes

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,13 @@
-const {Events, ActivityType} = require('discord.js');
-const logger = require('../utils/logger');
+const { Events, ActivityType } = require("discord.js");
+const logger = require("../utils/logger");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     logger.info(`Ready! Logged in as ${client.user.tag}`);
-    client.user.setActivity('OMORI', {type: ActivityType.Playing});
+    client.user.setActivity("Glad to serve you! (˶ᵔ ᵕ ᵔ˶)", {
+      type: ActivityType.Custom,
+    });
   },
 };
